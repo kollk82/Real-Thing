8551567957c1eb08f100786db0bbad719a516dd0
kollk82 <79942782+kollk82@users.noreply.github.com>
2021-07-15 02:52:03 +0100

    15/07/21

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,31 +1,102 @@
 <template>
     <div class="wrapper">
         <div class="portfolio">
-            <h1 class=" center vh-100">Portfolio</h1>
+            <h1 v-scrollanimation class=" center vh-100">Portfolio</h1>
             <div class="section">
                 <div class="section--box">
                     <div class="section--box-one">
-                        <h1>1.</h1>
+                        <h1 v-scrollanimation>1.</h1>
+                        <p v-scrollanimation>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Neque minus aspernatur voluptates adipisci
+                            tempora quas quis minima eveniet dolor fugit, odit
+                            voluptas nisi, enim recusandae. Eos eum dolorem modi
+                            magni?
+                        </p>
+                        <button v-scrollanimation class="btn">see more...</button>
+                    </div>
+                    <div class="section--box-two">
+                        <img v-scrollanimation src="img/imgs/logo.png" alt="logo">
                     </div>
                 </div>
                 <div class="section--box">
                     <div class="section--box-one">
-                        <h1>2.</h1>
+                        <h1 v-scrollanimation>2.</h1>
+                        <p v-scrollanimation>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Neque minus aspernatur voluptates adipisci
+                            tempora quas quis minima eveniet dolor fugit, odit
+                            voluptas nisi, enim recusandae. Eos eum dolorem modi
+                            magni?
+                        </p>
+                        <button v-scrollanimation class="btn">see more...</button>
                     </div>
-                </div>
-                <div class="section--box">
                     <div class="section--box-two">
-                        <h1>3.</h1>
+                        <img v-scrollanimation src="img/imgs/logo.png" alt="logo">
                     </div>
                 </div>
                 <div class="section--box">
-                    <div class="section--box-three">
-                        <h1>4.</h1>
+                    <div class="section--box-one">
+                        <h1 v-scrollanimation>3.</h1>
+                        <p v-scrollanimation>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Neque minus aspernatur voluptates adipisci
+                            tempora quas quis minima eveniet dolor fugit, odit
+                            voluptas nisi, enim recusandae. Eos eum dolorem modi
+                            magni?
+                        </p>
+                        <button v-scrollanimation class="btn">see more...</button>
+                    </div>
+                    <div class="section--box-two">
+                        <img v-scrollanimation src="img/imgs/logo.png" alt="logo">
                     </div>
                 </div>
                 <div class="section--box">
-                    <div class="section--box-four">
-                        <h1>5.</h1>
+                    <div class="section--box-one">
+                        <h1 v-scrollanimation>4.</h1>
+                        <p v-scrollanimation>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Neque minus aspernatur voluptates adipisci
+                            tempora quas quis minima eveniet dolor fugit, odit
+                            voluptas nisi, enim recusandae. Eos eum dolorem modi
+                            magni?
+                        </p>
+                        <button v-scrollanimation class="btn">see more...</button>
+                    </div>
+                    <div class="section--box-two">
+                       <img v-scrollanimation src="img/imgs/logo.png" alt="logo">
+                    </div>
+                </div>
+                <div class="section--box">
+                    <div class="section--box-one">
+                        <h1 v-scrollanimation>5.</h1>
+                        <p v-scrollanimation>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Neque minus aspernatur voluptates adipisci
+                            tempora quas quis minima eveniet dolor fugit, odit
+                            voluptas nisi, enim recusandae. Eos eum dolorem modi
+                            magni?
+                        </p>
+                        <button v-scrollanimation class="btn">see more...</button>
+                    </div>
+                    <div class="section--box-two">
+                        <img v-scrollanimation src="img/imgs/logo.png" alt="logo">
+                    </div>
+                </div>
+                <div class="section--box">
+                    <div class="section--box-one">
+                        <h1 v-scrollanimation>6.</h1>
+                        <p v-scrollanimation>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Neque minus aspernatur voluptates adipisci
+                            tempora quas quis minima eveniet dolor fugit, odit
+                            voluptas nisi, enim recusandae. Eos eum dolorem modi
+                            magni?
+                        </p>
+                        <button v-scrollanimation class="btn">see more...</button>
+                    </div>
+                    <div class="section--box-two">
+                       <img v-scrollanimation src="img/imgs/logo.png" alt="logo">
                     </div>
                 </div>
             </div>
